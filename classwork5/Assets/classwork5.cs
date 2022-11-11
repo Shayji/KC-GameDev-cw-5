0eea63b14e12abf51bb7525e7ef8962a856022da
Shayji <awrad.alshayji227@gmail.com>
2022-11-11 16:26:07 +0300

    classwork5

---
+++ classwork5/Assets/classwork5.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classwork5 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, 0f);
+    }
+}
